d5fdc70480e2c435de7b04ae09d7b85c1bf401ba
sgjy <1213353801@qq.com>
2020-11-05 16:14:41 +0800

    初始化项目

--- shop-demo/src/views/home/components/flashSale/FlashSale.vue
+++ shop-demo/src/views/home/components/flashSale/FlashSale.vue
@@ -5,19 +5,18 @@
  * @Date: 2020/11/5 15:21
 -->
 <template>
-    <div class="wrapper">
-        <div class="titleWrapper">
+    <div class="flashSale-wrap">
+        <div class="title-wrap">
             <div class="title">限时抢购</div>
             <span class="actionStateText">07:00场</span>
             <div class="actionStateTextEnd">已结束</div>
             <span class="more">更多</span>
         </div>
-        <div class="itemsWrapper">
+        <div class="flashSale-wrap_item-wrap">
             <FlashSaleItem
                     v-for="(product) in flashSaleList"
                     :product="product"
-                    :key="product.id"
-            />
+                    :key="product.id"/>
         </div>
     </div>
 </template>
@@ -41,70 +40,68 @@
     }
 </script>
 
-<style lang='css' scoped>
-    .wrapper {
+<style lang='scss' scoped>
+    .flashSale-wrap {
         position: relative;
         margin-top: 0.625rem;
         padding: 1.25rem 0 0.9375rem 0;
-        background: white; }
+        background: white;
+        .title-wrap {
+            position: relative;
+            margin: 0 0.625rem 0.625rem 0.625rem;
+            height: 1.5625rem;
+            .title {
+                display: inline-block;
+                border-left: 4px #3cb963 solid;
+                padding-left: 0.3125rem;
+                height: 1.3125rem;
+                line-height: 1.3125rem;
+                vertical-align: middle;
+                font-size: 1.1875rem;
+            }
+            .actionStateText {
+                display: inline-block;
+                vertical-align: middle;
+                line-height: 0.875rem;
+                font-size: 0.875rem;
+                border-radius: 3px;
+                padding: 0.15625rem;
+                /*border: 1px solid #FE3824;*/
+                color: #FE3824;
+                margin-left: 0.3125rem;
+            }
 
-    .titleWrapper {
-        position: relative;
-        margin: 0 0.625rem 0.625rem 0.625rem;
-        height: 1.5625rem;
-    }
+            .actionStateTextEnd {
+                display: inline-block;
+                vertical-align: middle;
+                line-height: 0.875rem;
+                font-size: 0.875rem;
+                border-radius: 3px;
+                padding: 0.15625rem;
+                border: 1px solid #999;
+                color: #666;
+                margin-left: 0.3125rem;
+            }
 
-    .title {
-        display: inline-block;
-        border-left: 4px #3cb963 solid;
-        padding-left: 0.3125rem;
-        height: 1.3125rem;
-        line-height: 1.3125rem;
-        vertical-align: middle;
-        font-size: 1.1875rem; }
-
-    .actionStateText {
-        display: inline-block;
-        vertical-align: middle;
-        line-height: 0.875rem;
-        font-size: 0.875rem;
-        border-radius: 3px;
-        padding: 0.15625rem;
-        /*border: 1px solid #FE3824;*/
-        color: #FE3824;
-        margin-left: 0.3125rem; }
-
-    .actionStateTextEnd {
-        display: inline-block;
-        vertical-align: middle;
-        line-height: 0.875rem;
-        font-size: 0.875rem;
-        border-radius: 3px;
-        padding: 0.15625rem;
-        border: 1px solid #999;
-        color: #666;
-        margin-left: 0.3125rem;
-    }
-
-    .more {
-        position: absolute;
-        top: 0;
-        right: 0;
-        height: 1.5625rem;
-        line-height: 1.5625rem;
-        font-size: 0.875rem;
-        color: #2EB257;
-    }
-
-    .itemsWrapper {
-        white-space: nowrap;
-        overflow-x: auto;
-        overflow-y: hidden;
-        -webkit-overflow-scrolling: touch;
-        overflow-scrolling: touch;
-    }
-
-    .itemsWrapper::-webkit-scrollbar{
-        display: none
+            .more {
+                position: absolute;
+                top: 0;
+                right: 0;
+                height: 1.5625rem;
+                line-height: 1.5625rem;
+                font-size: 0.875rem;
+                color: #2EB257;
+            }
+        }
+        &_item-wrap {
+            white-space: nowrap;
+            overflow-x: auto;
+            overflow-y: hidden;
+            -webkit-overflow-scrolling: touch;
+            overflow-scrolling: touch;
+        }
+        &_item-wrap::-webkit-scrollbar {
+            display: none;
+        }
     }
 </style>
--- shop-demo/src/views/home/components/flashSale/FlashSaleItem.vue
+++ shop-demo/src/views/home/components/flashSale/FlashSaleItem.vue
@@ -5,7 +5,7 @@
  * @Date: 2020/11/5 15:21
 -->
 <template>
-    <div class="wrapper" data-index="3">
+    <div class="flashSale-item-wrap" data-index="3">
         <div class="itemWrapper">
             <div class="imageWrapper">
                 <img class="image" :src="product.small_image" :alt="product.spec">
@@ -62,87 +62,84 @@
     }
 </script>
 
-<style lang='css' scoped>
-    .wrapper {
+<style lang='scss' scoped>
+    .flashSale-item-wrap {
         display: inline-block;
         padding: 0 0.3125rem;
         margin: 0 0.5rem;
         white-space: normal;
         width: 28%;
+        .imageWrapper {
+            height: 0;
+            padding-bottom: 100%;
+            overflow: hidden;
+            margin-bottom: 0.625rem;
+            position: relative;
+        }
+        .image {
+            width: 100%;
+            height: auto;
+            display: block;
+        }
+        .name {
+            margin-bottom: 0.625rem;
+            height: 2.5rem;
+            line-height: 1.25rem;
+            font-size: 0.8125rem;
+            word-break: break-all;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: -webkit-box;
+            -webkit-line-clamp: 2;
+            -webkit-box-orient: vertical;
+        }
+
+        .originPrice {
+            margin-bottom: 0.3125rem;
+            font-size: 0.6875rem;
+            color: #999999;
+            text-decoration: line-through;
+        }
+
+        .noticeSoldOut span {
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            -webkit-transform: translate(-50%, -50%);
+            transform: translate(-50%, -50%);
+            display: inline-block;
+            width: 60%;
+            text-align: center;
+            color: #FFF;
+            font-size: 0.75rem;
+            height: 1.25rem;
+            line-height: 1.25rem;
+            background: rgba(0, 0, 0, 0.4);
+            border-radius: 1.25rem;
+        }
+
+        .priceWrapper {
+            position: relative;
+        }
+
+        .price {
+            height: 1.875rem;
+            line-height: 1.875rem;
+            font-size: 0.6875rem;
+            color: #FE6263;
+        }
+
+        .iconCartWrapper {
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 1.875rem;
+        }
     }
 
     @media screen and (min-width: 0px) and (max-width: 320px) {
         .itemWrapper {
             width: 33%;
         }
-    }
-
-    .imageWrapper {
-        height: 0;
-        padding-bottom: 100%;
-        overflow: hidden;
-        margin-bottom: 0.625rem;
-        position: relative;
-    }
-
-    .image {
-        width: 100%;
-        height: auto;
-        display: block;
-    }
-
-    .name {
-        margin-bottom: 0.625rem;
-        height: 2.5rem;
-        line-height: 1.25rem;
-        font-size: 0.8125rem;
-        word-break: break-all;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-    }
-
-    .originPrice {
-        margin-bottom: 0.3125rem;
-        font-size: 0.6875rem;
-        color: #999999;
-        text-decoration: line-through;
-    }
-
-    .noticeSoldOut span {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        -webkit-transform: translate(-50%, -50%);
-        transform: translate(-50%, -50%);
-        display: inline-block;
-        width: 60%;
-        text-align: center;
-        color: #FFF;
-        font-size: 0.75rem;
-        height: 1.25rem;
-        line-height: 1.25rem;
-        background: rgba(0, 0, 0, 0.4);
-        border-radius: 1.25rem;
-    }
-
-    .priceWrapper {
-        position: relative;
-    }
-
-    .price {
-        height: 1.875rem;
-        line-height: 1.875rem;
-        font-size: 0.6875rem;
-        color: #FE6263;
-    }
-
-    .iconCartWrapper {
-        position: absolute;
-        top: 0;
-        right: 0;
-        width: 1.875rem;
     }
 </style>
